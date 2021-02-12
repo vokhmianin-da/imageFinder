0e7ba340f38b9288fb370d08660dd3d2eebe8faf
vokhmianin-da <ishchar@mail.ru>
2021-02-12 15:38:14 +0300

    Регулярки

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -42,11 +42,11 @@ void MainWindow::on_pushButton_clicked()    //вывод картинок
         temp = stream.readAll();
         htmlFile.close();
         // Проверить регулярку можно на сайте: https://regex101.com
-        QRegExp regex("<img([^(src)]+)src=\"([^(\")]+)\""); //
+        QRegExp regex("<img.*src=\"([^\"]+)\""); //regex("<img([^(src)]+)src=\"([^(\")]+)\"")
         int lastPos = 0;
         while( ( lastPos = regex.indexIn( temp, lastPos ) ) != -1 ) {
             lastPos += regex.matchedLength();
-            list.append(regex.cap(2));
+            list.append(regex.cap(1));
         }
 //       temp1 = getString(temp,"<img class=", ">");
 //       list.append(temp1);
@@ -61,8 +61,12 @@ void MainWindow::on_pushButton_clicked()    //вывод картинок
 //       x = temp.indexOf(temp1);
 
 //       ui->label1->setText(list[0]);
-        loader->download(list[0]);
+        temp = "//im0-tub-ru.yandex.net/i?id=420ae0d7f3620a053d7030b8c23f9c78&amp;n=13";
+        loader->download(temp);
         QPixmap pict("htmlFile.txt");
+        pict = pict.scaled(500, 400, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+        ui->label1->setPixmap(pict);
+        ui->label1->setFixedSize(pict.size());
         ui->label1->setPixmap(pict);
 
     }
